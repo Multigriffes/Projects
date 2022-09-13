5e10a551107ec85e913a9681992a9e0aa8251e38
Multigriffes <90653170+Multigriffes@users.noreply.github.com>
2022-09-13 21:20:46 +0200

    small correction

--- Python/Api/ApexLegendsStatus/Request_player.py
+++ Python/Api/ApexLegendsStatus/Request_player.py
@@ -2,13 +2,12 @@ import requests
 
 plateform = input("La plateform")
 player = input("Le joueur")
+reponse_player = requests.get("https://api.mozambiquehe.re/bridge?auth=95f0efbc14fc27681790be9963d1b212&player=" + (player) + "&platform=" + (plateform) + "&enableClubsBeta=true&merge=true")
 
-url = requests.get ("https://api.mozambiquehe.re/bridge?auth=95f0efbc14fc27681790be9963d1b212&player=$player&platform=$plateform&enableClubsBeta=true&merge=true")
-
-print = (url.status_code)
-
-if url.status_code == 200:
-    print(url.text)
+if reponse_player.status_code == 200:
+    print(reponse_player.text)
+    reponse = reponse_player.text
 else:
     print("Error")
 
+    
